736312dfdfb775a5b51e353df13967d7636f3280
Nicekode4 <45624383+Nicekode4@users.noreply.github.com>
2022-03-30 19:19:02 +0200

    Bustider og kantinen

---
+++ js/bustider.js
@@ -0,0 +1,15 @@
+
+
+                    $.getJSON('https://xmlopen.rejseplanen.dk/bin/rest.exe/multiDepartureBoard?id1=851400602&id2=8519734&format=json', function(data) {
+
+   var text = `${data.MultiDepartureBoard.Departure[0].name}`
+
+                        var text2 = `${data.MultiDepartureBoard.Departure[0].time}`
+
+                            var text3 = `${data.MultiDepartureBoard.Departure[0].time}`
+
+
+                            $(".busnavn").html(text);
+                            $(".bustid").html(text2);
+
+});
