fc74415cd10a18f448b5699102bd6566797763df
Nicekode4 <45624383+Nicekode4@users.noreply.github.com>
2022-03-31 20:43:11 +0200

    Startet og tilføjet cards

---
+++ bustider/js/script.js
@@ -0,0 +1,34 @@
+
+
+                    $.getJSON('https://xmlopen.rejseplanen.dk/bin/rest.exe/multiDepartureBoard?id1=851400602&id2=8519734&format=json', function(data) {
+
+                      var navn = `${data.MultiDepartureBoard.Departure[0].line}`
+                      var tid = `${data.MultiDepartureBoard.Departure[0].time}`
+                      var destination = `${data.MultiDepartureBoard.Departure[0].finalStop}`
+                      var HvorErDen = `${data.MultiDepartureBoard.Departure[0].stop}`
+                      var navn2 = `${data.MultiDepartureBoard.Departure[1].line}`
+                      var tid2 = `${data.MultiDepartureBoard.Departure[1].time}`
+                      var destination2 = `${data.MultiDepartureBoard.Departure[1].finalStop}`
+                      var HvorErDen2 = `${data.MultiDepartureBoard.Departure[1].stop}`
+                      var navn3 = `${data.MultiDepartureBoard.Departure[2].line}`
+                      var tid3 = `${data.MultiDepartureBoard.Departure[2].time}`
+                      var destination3 = `${data.MultiDepartureBoard.Departure[2].finalStop}`
+                      var HvorErDen3 = `${data.MultiDepartureBoard.Departure[2].stop}`
+                                           
+                                              
+                   
+                   
+                                               $(".busnavn").html(navn);
+                                               $(".bustid").html(tid);
+                                               $(".busdestination").html(destination);
+                                               $(".HvorHolderDen").html(HvorErDen);
+                                               $(".busnavn2").html(navn2);
+                                               $(".bustid2").html(tid2);
+                                               $(".busdestination2").html(destination2);
+                                               $(".HvorHolderDen2").html(HvorErDen2);
+                                               $(".busnavn3").html(navn3);
+                                               $(".bustid3").html(tid3);
+                                               $(".busdestination3").html(destination3);
+                                               $(".HvorHolderDen3").html(HvorErDen3);
+                   
+                   });
