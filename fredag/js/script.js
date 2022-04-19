a104d58680818eabeb29d6bd537499fe1e8e9f1b
Philip Thomsen <45624383+Nicekode4@users.noreply.github.com>
2022-04-19 10:16:28 +0200

    Update script.js

--- fredag/js/script.js
+++ fredag/js/script.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("April 1, 2022 14:15:00").getTime();
+var countDownDate = new Date("May 6, 2022 14:15:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
