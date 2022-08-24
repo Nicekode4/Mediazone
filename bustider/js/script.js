e818de55419e8472dcbaf9d36bbde1d9fbd781ce
Philip Thomsen <philip-th@live.dk>
2022-08-24 14:16:05 +0200

    Update script.js

--- bustider/js/script.js
+++ bustider/js/script.js
@@ -1,32 +1,18 @@
 
 
-                    $.getJSON('https://xmlopen.rejseplanen.dk/bin/rest.exe/multiDepartureBoard?id1=851400602&id2=851973402&format=json', function(data) {
+                    
+                   for (let index = 0; index < 10; index++) {
+                    $.getJSON('https://xmlopen.rejseplanen.dk/bin/rest.exe/multiDepartureBoard?id1=851400602&id2=851973402&id3=851400601&format=json', function(data) {
+                    let navn = `${data.MultiDepartureBoard.Departure[index].line}`
+                    let tid = `${data.MultiDepartureBoard.Departure[index].time}`
+                    let destination = `${data.MultiDepartureBoard.Departure[index].finalStop}`
+                    let HvorErDen = `${data.MultiDepartureBoard.Departure[index].stop}`
+                    $(".busnavn").html(navn);
+                    $(".bustid").html(tid);
+                    $(".busdestination").html(destination);
+                    $(".HvorHolderDen").html(HvorErDen);
+                    document.body.innerHTML = document.body.innerHTML + '<section class="Card-One"><article class="Card-One-Art-One"><h1>' + `${navn}` + '</h1><h3>' + `${HvorErDen}` + '</h3><h5>' + `${destination}` + '</h5></article><article class="Card-One-Art-Two"><h1>' + `${tid}` + '</h1></article></section>'
 
-                    let MyData = `${data}`
-                      var navn = `${data.MultiDepartureBoard.Departure[0].line}`
-                      var tid = `${data.MultiDepartureBoard.Departure[0].time}`
-                      var destination = `${data.MultiDepartureBoard.Departure[0].finalStop}`
-                      var HvorErDen = `${data.MultiDepartureBoard.Departure[0].stop}`
-console.log(`${data.MultiDepartureBoard}`);
-                      for (let index = 0; index < 3; index++) {
-                        document.body.innerHTML = '<section class=""><article class=""><h1 class="busnavn">Busnr</h1><h3 class="HvorHolderDen">Navn/hvor holder den</h3><h5 class="busdestination">Destination</h5></article><article class="Card-One-Art-Two"><h1 class="bustid">Tidspunkt</h1></article></section>'
-                        
-                      }
-                                           
-                                              
-                   
-                   
-                                               $(".busnavn").html(navn);
-                                               $(".bustid").html(tid);
-                                               $(".busdestination").html(destination);
-                                               $(".HvorHolderDen").html(HvorErDen);
-                                               $(".busnavn2").html(navn2);
-                                               $(".bustid2").html(tid2);
-                                               $(".busdestination2").html(destination2);
-                                               $(".HvorHolderDen2").html(HvorErDen2);
-                                               $(".busnavn3").html(navn3);
-                                               $(".bustid3").html(tid3);
-                                               $(".busdestination3").html(destination3);
-                                               $(".HvorHolderDen3").html(HvorErDen3);
-                   
                    });
+                                       
+                  }
