52f3cd8fa9be4ca760dd013130bf4773f7d4687e
Philip Thomsen <philip-th@live.dk>
2022-08-25 08:21:59 +0200

    Update bustider.js

--- js/bustider.js
+++ js/bustider.js
@@ -1,10 +1,7 @@
 
 
-<<<<<<< Updated upstream
                     $.getJSON('https://xmlopen.rejseplanen.dk/bin/rest.exe/multiDepartureBoard?id1=851400602&id2=851973402&format=json', function(data) {
-=======
-                    $.getJSON('https://xmlopen.rejseplanen.dk/bin/rest.exe/multiDepartureBoard?id1=851400602&id2=851973402&id3=851400601&format=json', function(data) {
->>>>>>> Stashed changes
+
 
    var text = `${data.MultiDepartureBoard.Departure[0].name}`
 
