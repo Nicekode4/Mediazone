503e69457d1bd4a1e061539aa402fbb4541deeb4
Philip Thomsen <philip-th@live.dk>
2022-08-31 11:39:07 +0200

    Update Kantinen.js

--- js/Kantinen.js
+++ js/Kantinen.js
@@ -4,7 +4,7 @@ let dayOfWeekNumber = date.getDay();
 currentdate = new Date();
 let oneJan = new Date(currentdate.getFullYear(),0,1);
 let numberOfDays = Math.floor((currentdate - oneJan) / (24 * 60 * 60 * 1000));
-let result = Math.ceil(( currentdate.getDay() + 1 + numberOfDays) / 7);
+let result = Math.ceil(( currentdate.getDay() + numberOfDays) / 7);
 let menu = JSON.parse(window.localStorage.getItem('canteen'))
 console.log(menu);
 if (window.localStorage.getItem('canteen') === null) {
