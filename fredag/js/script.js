3f8be4a6a3f77da8598dce379a3bbf057408105d
Nicekode4 <45624383+Nicekode4@users.noreply.github.com>
2022-09-03 23:45:01 +0200

    Update script.js

--- fredag/js/script.js
+++ fredag/js/script.js
@@ -4,28 +4,17 @@ let event = document.querySelector('.socialArtOne')
 let dato = document.querySelector('.socialArtTwo')
 let favi = "https://s2.googleusercontent.com/s2/favicons?domain_url=https://www.stackoverflow.com"
 let array1;
-let data1;
-let test = "\"Start\\nFredagsbar - 02.09\\nIT Dayz - 11.09\\nNoget - 12.09\\nSlut\""
 // As with JSON, use the Fetch API & ES6
 fetch('events.txt')
   .then(response => response.text())
   .then(data => {
-    // console.log();
     array1 = JSON.stringify(data).split("\\n")
     array1.shift(); // removes first element
     array1.pop(); // removes last element
-    
-    console.log(array1);
-    
     for (let index = 0; index < array1.length; index++) {
-let array2 = array1[index].split("-");
+    let array2 = array1[index].split("-");
     event.innerHTML = '<p>' + event.innerHTML + array2[0] + '</p>'
     dato.innerHTML = '<p>' + dato.innerHTML + array2[1] + '</p>'
-}
-  setTimeout(() => {
-    
-
-  }, 1000);
-    
+}  
   });
  
