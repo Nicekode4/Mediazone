14e2f75fb3fcefa40b468090bc5065f914325dd3
Nicekode4 <45624383+Nicekode4@users.noreply.github.com>
2022-09-06 19:39:31 +0200

    Bug fixes

--- js/Kantinen.js
+++ js/Kantinen.js
@@ -6,31 +6,12 @@ let oneJan = new Date(currentdate.getFullYear(),0,1);
 let numberOfDays = Math.floor((currentdate - oneJan) / (24 * 60 * 60 * 1000));
 let result = Math.ceil(( currentdate.getDay() + numberOfDays) / 7);
 let menu = JSON.parse(window.localStorage.getItem('canteen'))
-console.log(menu.Week);
-console.log(result);
-//console.log(result);
-if (window.localStorage.getItem('canteen') === null) {
-    $.getJSON('https://infoskaerm.techcollege.dk/umbraco/api/content/getcanteenmenu/?format=json', function(data) {
-        window.localStorage.setItem('canteen', JSON.stringify(data))
-        })
-}
-console.log(typeof document.querySelector('#dish').innerHTML);
-
-function canteen() {
-     $.getJSON('https://infoskaerm.techcollege.dk/umbraco/api/content/getcanteenmenu/?format=json', function(data) {
-        window.localStorage.setItem('canteen', JSON.stringify(data))   
-})
-window.location.href=window.location.href
-}
-        
-        
-
-
-if (result != menu.Week) {
-
-    
+let today = window.localStorage.getItem('today')
+console.log(today);
+if (today != dayOfWeekNumber) {
+    canteen()
+    window.localStorage.setItem('today', dayOfWeekNumber) ;
 }else{
-    
     switch(dayOfWeekNumber){
         case 0: 
             nameOfDay = 'Sunday';
@@ -64,6 +45,47 @@ if (result != menu.Week) {
     }
 }
 
+function canteen() {
+     $.getJSON('https://infoskaerm.techcollege.dk/umbraco/api/content/getcanteenmenu/?format=json', function(data) {
+        window.localStorage.setItem('canteen', JSON.stringify(data))   
+        switch(dayOfWeekNumber){
+            case 0: 
+                nameOfDay = 'Sunday';
+                document.querySelector('#dish').innerHTML = 'Du har weekend <3';
+                break;
+            case 1:
+                nameOfDay = 'Monday';
+                document.querySelector('#dish').innerHTML = menu.Days[dayOfWeekNumber - 1].Dish
+                break;
+            case 2:
+                nameOfDay = 'Tuesday';
+                document.querySelector('#dish').innerHTML = menu.Days[dayOfWeekNumber - 1].Dish
+                break;
+            case 3:
+                nameOfDay = 'Wednesday';
+                document.querySelector('#dish').innerHTML = menu.Days[dayOfWeekNumber - 1].Dish
+                break;
+            case 4:
+                nameOfDay = 'Thursday';
+                document.querySelector('#dish').innerHTML = menu.Days[dayOfWeekNumber - 1].Dish
+                break;
+            case 5:
+                nameOfDay = 'Friday';
+                document.querySelector('#dish').innerHTML = menu.Days[dayOfWeekNumber - 1].Dish
+                break;
+            case 6:
+                nameOfDay = 'Saturday';
+                document.querySelector('#dish').innerHTML = 'Du har weekend <3';
+                break;
+        
+        }
+})
+
+}
+
+window.addEventListener('drag', function () {
+    alert('hi')
+})
 
 
 
---
+++ yderligere/js/script.js
@@ -0,0 +1,20 @@
+let container = document.querySelector('.grid-container')
+let url = "https://s2.googleusercontent.com/s2/favicons?domain_url="
+// As with JSON, use the Fetch API & ES6
+fetch('spil.txt')
+  
+  .then(response => response.text())
+  .then(data => {
+  	let array1 = JSON.stringify(data).split("\\n")
+    array1.shift(); // removes first element
+    array1.pop(); // removes last element
+    for (let index = 0; index < array1.length; index++) {
+          let array2 = array1[index].split("-");
+
+  	     container.innerHTML = container.innerHTML + '<div class="grid-item"><a href="' + array2[0] + '"target="_blank" rel="noopener noreferrer"><img class="gridImg" style="height: 100px;" src="' + url + array2[0] + '" alt="App Icon" ></a><figcaption>' + array2[1] + '</figcaption></div>'
+    }
+
+  });
+//https://s2.googleusercontent.com/s2/favicons?domain_url=https://outlook.live.com
+
+  //<div class="grid-item"><img class="gridImg" src="./assests/APP.png" alt="App Icon" ><figcaption>app navn</figcaption></div>
