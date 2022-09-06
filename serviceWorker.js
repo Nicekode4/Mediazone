457d8e85ade080cec92558be48fc8da5d7d8d3ec
Philip Thomsen <philip-th@live.dk>
2022-09-06 14:42:07 +0200

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,14 +1,25 @@
-const staticDevCoffee = "calc-site-v1";
+const staticMediazone = "Mediazone-site-v1";
 const assets = [
   "/",
   "/index.html",
-  "/assets/stylesheet.css",
-  "/assets/script.js"
+  "/Stylesheets\main.css",
+  "/js/bustider.js",
+  "/js/kantinen.js",
+  "/fredag/stylesheets/main.css",
+  "/fredag/js/script.js",
+  "/fredag/games/js/script.js",
+  "/fredag/games/index.html",
+  "/fredag/index.html",
+  "/bustider/index.html",
+  "/bustider/js/script.js",
+  "/fredag/games/spil.txt",
+  "/fredag/events.txt",
+
 ];
 
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
-    caches.open(staticDevCoffee).then(cache => {
+    caches.open(staticMediazone).then(cache => {
       cache.addAll(assets);
     })
   );
