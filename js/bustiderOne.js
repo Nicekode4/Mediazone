de96610c308c94a0955219d0cd945ac782c9166c
Philip Thomsen <philip-th@live.dk>
2022-09-29 13:20:04 +0200

    Update bustiderOne.js

--- js/bustiderOne.js
+++ js/bustiderOne.js
@@ -10,11 +10,23 @@
                             $(".busnavn").html(text);
                             $(".bustid").html(text2);
                             });
-}else{
-                        let text = `${data.MultiDepartureBoard.Departure[0].name}`
-                        let text2 = `${data.MultiDepartureBoard.Departure[0].time}`
-                        $(".busnavn").html(text);
-                        $(".bustid").html(text2);
+}else{          
+    if (data.MultiDepartureBoard.Departure[0].rtTime = 'undefined') {
+        let text = `${data.MultiDepartureBoard.Departure[0].name}`
+        let text2 = `${data.MultiDepartureBoard.Departure[0].time}`
+        $(".busnavn").html(text);
+        $(".bustid").html(text2);
+        }else{
+            let text = `${data.MultiDepartureBoard.Departure[0].name}`
+            let a = `${data.MultiDepartureBoard.Departure[0].rtTime}`.replace(':', '')
+            let b = `${data.MultiDepartureBoard.Departure[0].time}`.replace(':', '')
+            console.log(b);
+            let text2 = `${data.MultiDepartureBoard.Departure[0].time}` + ' + ' + parseInt(a-b)
+            
+            $(".busnavn").html(text);
+            $(".bustid").html(text2);
+        }
+
                         }
                             
                             
