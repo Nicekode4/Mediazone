7347395a418c4199c81d64fac06f6e3b6150dcd1
Nicekode4 <45624383+Nicekode4@users.noreply.github.com>
2023-02-13 14:07:20 +0100

    Update bustiderOne.js

--- js/bustiderOne.js
+++ js/bustiderOne.js
@@ -1,23 +1,525 @@
+const skema = [
+    {
+        "Team": "ggr010123",
+        "StartDate": "2023-02-10T07:15:00+01:00",
+        "Subject": "Graf.tekn.",
+        "Education": "Grafisk teknik.",
+        "Room": "N120"
+    },
+    {
+        "Team": "ggr010123",
+        "StartDate": "2023-02-10T08:20:00+01:00",
+        "Subject": "Graf.tekn.",
+        "Education": "Grafisk teknik.",
+        "Room": "N120"
+    },
+    {
+        "Team": "ggr010123",
+        "StartDate": "2023-02-10T09:30:00+01:00",
+        "Subject": "Graf.tekn.",
+        "Education": "Grafisk teknik.",
+        "Room": "N120"
+    },
+    {
+        "Team": "ggr010123",
+        "StartDate": "2023-02-10T11:00:00+01:00",
+        "Subject": "Graf.tekn.",
+        "Education": "Grafisk teknik.",
+        "Room": "N120"
+    },
+    {
+        "Team": "ggr010123",
+        "StartDate": "2023-02-10T12:00:00+01:00",
+        "Subject": "Graf.tekn.",
+        "Education": "Grafisk teknik.",
+        "Room": "N120"
+    },
+    {
+        "Team": "h0gr020123",
+        "StartDate": "2023-02-10T07:15:00+01:00",
+        "Subject": "digitaltryk",
+        "Education": "Grafisk teknik.",
+        "Room": "N112b"
+    },
+    {
+        "Team": "h0gr020123",
+        "StartDate": "2023-02-10T08:20:00+01:00",
+        "Subject": "digitaltryk",
+        "Education": "Grafisk teknik.",
+        "Room": "N112b"
+    },
+    {
+        "Team": "h0gr020123",
+        "StartDate": "2023-02-10T09:30:00+01:00",
+        "Subject": "digitaltryk",
+        "Education": "Grafisk teknik.",
+        "Room": "N112b"
+    },
+    {
+        "Team": "h0gr020123",
+        "StartDate": "2023-02-10T11:00:00+01:00",
+        "Subject": "digitaltryk",
+        "Education": "Grafisk teknik.",
+        "Room": "N112b"
+    },
+    {
+        "Team": "h0gr020123",
+        "StartDate": "2023-02-10T12:00:00+01:00",
+        "Subject": "digitaltryk",
+        "Education": "Grafisk teknik.",
+        "Room": "N112b"
+    },
+    {
+        "Team": "h0gr020223",
+        "StartDate": "2023-02-10T07:15:00+01:00",
+        "Subject": "prod storf prin",
+        "Education": "Grafisk teknik.",
+        "Room": "N112"
+    },
+    {
+        "Team": "h0gr020223",
+        "StartDate": "2023-02-10T08:20:00+01:00",
+        "Subject": "prod storf prin",
+        "Education": "Grafisk teknik.",
+        "Room": "N112"
+    },
+    {
+        "Team": "h0gr020223",
+        "StartDate": "2023-02-10T09:30:00+01:00",
+        "Subject": "prod storf prin",
+        "Education": "Grafisk teknik.",
+        "Room": "N112"
+    },
+    {
+        "Team": "h0gr020223",
+        "StartDate": "2023-02-10T11:00:00+01:00",
+        "Subject": "prod storf prin",
+        "Education": "Grafisk teknik.",
+        "Room": "N112"
+    },
+    {
+        "Team": "h0gr020223",
+        "StartDate": "2023-02-10T12:00:00+01:00",
+        "Subject": "prod storf prin",
+        "Education": "Grafisk teknik.",
+        "Room": "N112"
+    },
+    {
+        "Team": "ltd010123e",
+        "StartDate": "2023-02-10T07:30:00+01:00",
+        "Subject": "Dansk",
+        "Education": "Brobyg teknisk",
+        "Room": "E101"
+    },
+    {
+        "Team": "ltd010123e",
+        "StartDate": "2023-02-10T08:30:00+01:00",
+        "Subject": "Dansk",
+        "Education": "Brobyg teknisk",
+        "Room": "E101"
+    },
+    {
+        "Team": "ltd010123e",
+        "StartDate": "2023-02-10T09:30:00+01:00",
+        "Subject": "Dansk",
+        "Education": "Brobyg teknisk",
+        "Room": "E101"
+    },
+    {
+        "Team": "ltd010123e",
+        "StartDate": "2023-02-10T10:30:00+01:00",
+        "Subject": "Dansk",
+        "Education": "Brobyg teknisk",
+        "Room": "E101"
+    },
+    {
+        "Team": "ltd010123e",
+        "StartDate": "2023-02-10T11:30:00+01:00",
+        "Subject": "Dansk",
+        "Education": "Brobyg teknisk",
+        "Room": "E101"
+    },
+    {
+        "Team": "ltd010123e",
+        "StartDate": "2023-02-10T12:30:00+01:00",
+        "Subject": "Dansk",
+        "Education": "Brobyg teknisk",
+        "Room": "E101"
+    },
+    {
+        "Team": "ltd010123e",
+        "StartDate": "2023-02-10T13:30:00+01:00",
+        "Subject": "Dansk",
+        "Education": "Brobyg teknisk",
+        "Room": "E101"
+    },
+    {
+        "Team": "gmg010123",
+        "StartDate": "2023-02-10T07:15:00+01:00",
+        "Subject": "Mediegrafiker",
+        "Education": "Mediegrafiker",
+        "Room": "P027-4"
+    },
+    {
+        "Team": "gmg010123",
+        "StartDate": "2023-02-10T08:20:00+01:00",
+        "Subject": "Mediegrafiker",
+        "Education": "Mediegrafiker",
+        "Room": "P027-4"
+    },
+    {
+        "Team": "gmg010123",
+        "StartDate": "2023-02-10T09:30:00+01:00",
+        "Subject": "Mediegrafiker",
+        "Education": "Mediegrafiker",
+        "Room": "P027-4"
+    },
+    {
+        "Team": "gmg010123",
+        "StartDate": "2023-02-10T11:00:00+01:00",
+        "Subject": "Mediegrafiker",
+        "Education": "Mediegrafiker",
+        "Room": "P027-4"
+    },
+    {
+        "Team": "gmg010123",
+        "StartDate": "2023-02-10T12:00:00+01:00",
+        "Subject": "Kl.time",
+        "Education": "Mediegrafiker",
+        "Room": "P027-4"
+    },
+    {
+        "Team": "h1mg010123",
+        "StartDate": "2023-02-10T07:15:00+01:00",
+        "Subject": "typografi",
+        "Education": "Mediegrafiker",
+        "Room": "P016"
+    },
+    {
+        "Team": "h1mg010123",
+        "StartDate": "2023-02-10T08:20:00+01:00",
+        "Subject": "typografi",
+        "Education": "Mediegrafiker",
+        "Room": "P016"
+    },
+    {
+        "Team": "h1mg010123",
+        "StartDate": "2023-02-10T09:30:00+01:00",
+        "Subject": "typografi",
+        "Education": "Mediegrafiker",
+        "Room": "P016"
+    },
+    {
+        "Team": "h1mg010123",
+        "StartDate": "2023-02-10T11:00:00+01:00",
+        "Subject": "typografi",
+        "Education": "Mediegrafiker",
+        "Room": "P016"
+    },
+    {
+        "Team": "h1mg010123",
+        "StartDate": "2023-02-10T12:00:00+01:00",
+        "Subject": "typografi",
+        "Education": "Mediegrafiker",
+        "Room": "P016"
+    },
+    {
+        "Team": "gwe010123",
+        "StartDate": "2023-02-10T07:15:00+01:00",
+        "Subject": "HTML5/CSS",
+        "Education": "Webudvikler",
+        "Room": "P024"
+    },
+    {
+        "Team": "gwe010123",
+        "StartDate": "2023-02-10T08:20:00+01:00",
+        "Subject": "HTML5/CSS",
+        "Education": "Webudvikler",
+        "Room": "P024"
+    },
+    {
+        "Team": "gwe010123",
+        "StartDate": "2023-02-10T09:30:00+01:00",
+        "Subject": "HTML5/CSS",
+        "Education": "Webudvikler",
+        "Room": "P024"
+    },
+    {
+        "Team": "gwe010123",
+        "StartDate": "2023-02-10T11:00:00+01:00",
+        "Subject": "Kl.time",
+        "Education": "Webudvikler",
+        "Room": "P024"
+    },
+    {
+        "Team": "gwe010123",
+        "StartDate": "2023-02-10T12:00:00+01:00",
+        "Subject": "HTML5/CSS",
+        "Education": "Webudvikler",
+        "Room": "P024"
+    },
+    {
+        "Team": "gsm010523",
+        "StartDate": "2023-02-10T07:15:00+01:00",
+        "Subject": "Bonusfag",
+        "Education": "Smed",
+        "Room": "SU211"
+    },
+    {
+        "Team": "gsm010523",
+        "StartDate": "2023-02-10T08:20:00+01:00",
+        "Subject": "Bonusfag",
+        "Education": "Smed",
+        "Room": "SU211"
+    },
+    {
+        "Team": "gsm010523",
+        "StartDate": "2023-02-10T09:30:00+01:00",
+        "Subject": "Bonusfag",
+        "Education": "Smed",
+        "Room": "SU211"
+    },
+    {
+        "Team": "gsm010523",
+        "StartDate": "2023-02-10T11:00:00+01:00",
+        "Subject": "Bonusfag",
+        "Education": "Smed",
+        "Room": "SU211"
+    },
+    {
+        "Team": "h0mg010123f",
+        "StartDate": "2023-02-10T07:15:00+01:00",
+        "Subject": "Praktik",
+        "Education": "Mediegrafiker",
+        "Room": ""
+    },
+    {
+        "Team": "h0mg010123f",
+        "StartDate": "2023-02-10T08:20:00+01:00",
+        "Subject": "Praktik",
+        "Education": "Mediegrafiker",
+        "Room": ""
+    },
+    {
+        "Team": "h0mg010123f",
+        "StartDate": "2023-02-10T09:30:00+01:00",
+        "Subject": "Praktik",
+        "Education": "Mediegrafiker",
+        "Room": ""
+    },
+    {
+        "Team": "h0mg010123f",
+        "StartDate": "2023-02-10T11:00:00+01:00",
+        "Subject": "Praktik",
+        "Education": "Mediegrafiker",
+        "Room": ""
+    },
+    {
+        "Team": "h0gr010123f",
+        "StartDate": "2023-02-10T07:00:00+01:00",
+        "Subject": "Praktik",
+        "Education": "Grafisk teknik.",
+        "Room": ""
+    },
+    {
+        "Team": "h0gr010123f",
+        "StartDate": "2023-02-10T08:20:00+01:00",
+        "Subject": "Praktik",
+        "Education": "Grafisk teknik.",
+        "Room": ""
+    },
+    {
+        "Team": "h0gr010123f",
+        "StartDate": "2023-02-10T09:30:00+01:00",
+        "Subject": "Praktik",
+        "Education": "Grafisk teknik.",
+        "Room": ""
+    },
+    {
+        "Team": "h0gr010123f",
+        "StartDate": "2023-02-10T11:00:00+01:00",
+        "Subject": "Praktik",
+        "Education": "Grafisk teknik.",
+        "Room": ""
+    },
+    {
+        "Team": "h1we010122",
+        "StartDate": "2023-02-10T07:15:00+01:00",
+        "Subject": "Praktisk web",
+        "Education": "Data/komm.udd.",
+        "Room": "P015"
+    },
+    {
+        "Team": "h1we010122",
+        "StartDate": "2023-02-10T08:20:00+01:00",
+        "Subject": "Praktisk web",
+        "Education": "Data/komm.udd.",
+        "Room": "P015"
+    },
+    {
+        "Team": "h1we010122",
+        "StartDate": "2023-02-10T09:30:00+01:00",
+        "Subject": "Praktisk web",
+        "Education": "Data/komm.udd.",
+        "Room": "P015"
+    },
+    {
+        "Team": "h1we010122",
+        "StartDate": "2023-02-10T11:00:00+01:00",
+        "Subject": "Praktisk web",
+        "Education": "Data/komm.udd.",
+        "Room": "P015"
+    },
+    {
+        "Team": "h1we010122",
+        "StartDate": "2023-02-10T12:00:00+01:00",
+        "Subject": "Praktisk web",
+        "Education": "Data/komm.udd.",
+        "Room": "P015"
+    },
+    {
+        "Team": "h1we080122",
+        "StartDate": "2023-02-10T07:15:00+01:00",
+        "Subject": "Praktisk web",
+        "Education": "Webudvikler",
+        "Room": "P026"
+    },
+    {
+        "Team": "h1we080122",
+        "StartDate": "2023-02-10T08:20:00+01:00",
+        "Subject": "Praktisk web",
+        "Education": "Webudvikler",
+        "Room": "P026"
+    },
+    {
+        "Team": "h1we080122",
+        "StartDate": "2023-02-10T09:30:00+01:00",
+        "Subject": "Praktisk web",
+        "Education": "Webudvikler",
+        "Room": "P026"
+    },
+    {
+        "Team": "h1we080122",
+        "StartDate": "2023-02-10T11:00:00+01:00",
+        "Subject": "Praktisk web",
+        "Education": "Webudvikler",
+        "Room": "P026"
+    },
+    {
+        "Team": "h1we080122",
+        "StartDate": "2023-02-10T12:00:00+01:00",
+        "Subject": "Praktisk web",
+        "Education": "Webudvikler",
+        "Room": "P026"
+    },
+    {
+        "Team": "h1we010223",
+        "StartDate": "2023-02-10T07:15:00+01:00",
+        "Subject": "Avanceret CSS",
+        "Education": "Webudvikler",
+        "Room": "P025"
+    },
+    {
+        "Team": "h1we010223",
+        "StartDate": "2023-02-10T08:20:00+01:00",
+        "Subject": "Avanceret CSS",
+        "Education": "Webudvikler",
+        "Room": "P025"
+    },
+    {
+        "Team": "h1we010223",
+        "StartDate": "2023-02-10T09:30:00+01:00",
+        "Subject": "Avanceret CSS",
+        "Education": "Webudvikler",
+        "Room": "P025"
+    },
+    {
+        "Team": "h1we010223",
+        "StartDate": "2023-02-10T11:00:00+01:00",
+        "Subject": "Avanceret CSS",
+        "Education": "Webudvikler",
+        "Room": "P025"
+    },
+    {
+        "Team": "h1we010223",
+        "StartDate": "2023-02-10T12:00:00+01:00",
+        "Subject": "Avanceret CSS",
+        "Education": "Webudvikler",
+        "Room": "P025"
+    },
+    {
+        "Team": "htxb21",
+        "StartDate": "2023-02-10T09:30:00+01:00",
+        "Subject": "Studieomr/proj",
+        "Education": "HTX",
+        "Room": "F235"
+    },
+    {
+        "Team": "htxb21",
+        "StartDate": "2023-02-10T11:00:00+01:00",
+        "Subject": "Studieomr/proj",
+        "Education": "HTX",
+        "Room": "F235"
+    },
+    {
+        "Team": "htxb21",
+        "StartDate": "2023-02-10T12:10:00+01:00",
+        "Subject": "Studieomr/proj",
+        "Education": "HTX",
+        "Room": "F235"
+    },
+    {
+        "Team": "htxb21",
+        "StartDate": "2023-02-10T07:15:00+01:00",
+        "Subject": "Studieomr/proj",
+        "Education": "HTX",
+        "Room": "F235"
+    },
+    {
+        "Team": "htxb21",
+        "StartDate": "2023-02-10T08:20:00+01:00",
+        "Subject": "Studieomr/proj",
+        "Education": "HTX",
+        "Room": "F235"
+    }
+]
+let skema2 = []
+for (let index = 0; index < skema.length; index++) {
+    const element = skema[index];
+    if (new Date().getMinutes() > 21 && new Date().getMinutes() < 59) {
+          if (new Date(element.StartDate).getHours() === new Date().getHours() - 3) {
+        skema2.push(element)
+    }  
+    } else if (new Date().getMinutes() < 20 && new Date().getMinutes() > 00 ) {
+        if (new Date(element.StartDate).getHours() === new Date().getHours() - 2) {
+            skema2.push(element)
+        }  
+    }
+
+}
+console.log(skema2);
 
 
+
+    console.log(skema2.length);                
                     $.getJSON('https://xmlopen.rejseplanen.dk/bin/rest.exe/multiDepartureBoard?id1=851400602&format=json', function(data) {
                     console.log(data);
                     if (data.MultiDepartureBoard.error == "No journeys found") {
                         $.getJSON('https://xmlopen.rejseplanen.dk/bin/rest.exe/multiDepartureBoard?id1=851400602&id2=851973402&id3=851400601&format=json', function(data) {
                     
-                        let text = `${data.MultiDepartureBoard.Departure[0].name}`
+                        let text = `${data.MultiDepartureBoard.Departure[0].name} ${skema2.length > 7 ? " | Lille chance for at få en plads" : ""}`
                         let text2 = `${data.MultiDepartureBoard.Departure[0].time}`
                             $(".busnavn").html(text);
                             $(".bustid").html(text2);
                             });
 }else{          
     if (typeof data.MultiDepartureBoard.Departure[0].rtTime === 'undefined') {
-        let text = `${data.MultiDepartureBoard.Departure[0].name}`
+        let text = `${data.MultiDepartureBoard.Departure[0].name} ${skema2.length > 7 ? " | Lille chance for at få en plads" : ""}`
         let text2 = `${data.MultiDepartureBoard.Departure[0].time}`
         $(".busnavn").html(text);
         $(".bustid").html(text2);
         }else{
-            let text = `${data.MultiDepartureBoard.Departure[0].name}`
+            let text = `${data.MultiDepartureBoard.Departure[0].name} ${skema2.length > 7 ? " | Lille chance for at få en plads" : ""}`
             let a = unixConvert(data.MultiDepartureBoard.Departure[index].rtTime)
             let b = unixConvert(data.MultiDepartureBoard.Departure[index].time)
             console.log(b);
@@ -32,7 +534,7 @@
                             
 
 });
-
+console.log(skema);
 function unixConvert(time) {
     const dateStr = `1970-01-01 ${time}`;
 
